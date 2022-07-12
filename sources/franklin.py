734a8e82787b5b75c7b029d25ee53930292a0ee6
David van Zessen <d.vanzessen@erasmusmc.nl>
2022-07-12 16:20:49 +0200

    updated franklin to async

--- sources/franklin.py
+++ sources/franklin.py
@@ -40,34 +40,51 @@ def Franklin(variant: dict, request) -> str:
         "card.html.jinja2", 
     ).render(title="Franklin", text="NCBI down?", subtitle="?", links=[{"url": url, "text": "Go"}])
 
-async def Franklin_rs(session: aiohttp.ClientSession, variant: dict) -> dict:
-    url = ""
-    rs = variant["rs"]
+async def get_parse_search_response(session: aiohttp.ClientSession, search: str) -> dict:
     url = f"https://franklin.genoox.com/api/parse_search"
-    new_variant_data = {}
 
     post_data = {
         "reference_version": "hg19",
-        "search_text_input": "rs397515359"
+        "search_text_input": search
     }
     headers = {
         'Content-Type': "application/json",
         'Cache-Control': "no-cache",
     }
+
+    async with session.post(url, json=post_data, headers=headers) as response:
+        resp = await response.json()
+        return resp
+
+async def get_classification_response(session: aiohttp.ClientSession, chrom, pos, ref, alt) -> dict:
+    url = f"https://franklin.genoox.com/api/classify"
+    post_data = {
+        "is_versioned_request":False,
+        "variant":{
+            "chrom":chrom,
+            "pos": pos,
+            "ref":ref,
+            "alt":alt,
+            "reference_version":"hg19"
+        }
+    }
+    headers = {
+        'Content-Type': "application/json",
+        'Cache-Control': "no-cache",
+    }
+    async with session.post(url, json=post_data, headers=headers) as response:
+        resp = await response.json()
+        return resp
+
+async def Franklin_rs(session: aiohttp.ClientSession, variant: dict) -> dict:
+    url = ""
+    rs = variant["rs"]
+    new_variant_data = {}
+
+    response_json = await get_parse_search_response(session, rs)
     
-    response = requests.post(url, json=post_data, headers=headers)
-
-    if response.status_code != 200:
-        html = templates.get_template(
-            "card.html.jinja2", 
-        ).render(title="Franklin", text=response.text, subtitle=f"{response.status_code}", links=[{"url": url, "text": "Go"}])
-        return SourceResult("Franklin", new_variant_data, html, False, error=f"'{url}' returned {response.status_code}")
-
-    response_json = response.json()
-
-    classification = "tmp"
-
     if "best_variant_option" in response_json:
+        
         variant = response_json["best_variant_option"]
         new_variant_data["chr"] = variant["chrom"].replace("chr", "")
         new_variant_data["pos"] = variant["pos"]
@@ -77,10 +94,22 @@ async def Franklin_rs(session: aiohttp.ClientSession, variant: dict) -> dict:
         full_variant = variant["to_full_variant"]
         new_variant_data["start"] = full_variant["start"]
         new_variant_data["end"] = full_variant["end"]
+        
+        url = f"https://franklin.genoox.com/clinical-db/variant/snp/{variant['chrom']}-{variant['pos']}-{variant['ref']}-{variant['alt']}"
+
+        classification_json = await get_classification_response(
+            session, 
+            new_variant_data["chr"], 
+            new_variant_data["pos"], 
+            new_variant_data["ref"], 
+            new_variant_data["alt"]
+        )
+
+        new_variant_data["franklin_classification"] = classification_json.get("classification", "Unknown")
     
     html = templates.get_template(
         "card.html.jinja2", 
-    ).render(title="Franklin", text=classification, subtitle="", links=[{"url": url, "text": "Go"}])
+    ).render(title="Franklin", text=new_variant_data.get("franklin_classification", "NA"), subtitle="", links=[{"url": url, "text": "Go"}])
     return SourceResult("Franklin", new_variant_data, html, True)
 
 Franklin_entries = {
