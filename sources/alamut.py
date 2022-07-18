8e5541fa382ab2f184e467d3c963e6b210cb2f7a
David van Zessen <d.vanzessen@erasmusmc.nl>
2022-07-18 10:33:44 +0200

    useless alamut /annotate code

--- sources/alamut.py
+++ sources/alamut.py
@@ -36,12 +36,30 @@ class Alamut(Source):
         if "transcript" in self.variant and "cdot" in self.variant:
             transcript = self.variant["transcript"]
             cdot = self.variant["cdot"]
-            url = f"http://127.0.0.1:10000/search?institution=ANL0016&apikey=18565976&request={transcript}:{cdot}"
+            transcript_cdot = f"{transcript}:{cdot}"
+            url = f"http://127.0.0.1:10000/search?institution=ANL0016&apikey=18565976&request={transcript_cdot}"
             urls.append({
                 "url": url, "text": "Transcript:cdot"
             })
-
+            """
+            print(f"http://127.0.0.1:10000/annotate?institution=ANL0016&apikey=18565976&variant={transcript_cdot}")
+            alamut = await self.async_get_json(f"http://127.0.0.1:10000/annotate?institution=ANL0016&apikey=18565976&variant={transcript_cdot}")
             
+            self.new_variant_data["alamut_classification"] = alamut["Classification"]
+            self.new_variant_data["clinvar_id"] = alamut["Clinvar Id"]
+            self.new_variant_data["cosmic_id"] = alamut["Cosmic Id"]
+            self.new_variant_data["cosmic_id"] = alamut["Cosmic Id"]
+            self.new_variant_data["gene"] = alamut["Gene"]
+            self.new_variant_data["hgnc_gene_id"] = alamut["HGNC Gene Id"]
+            self.new_variant_data["hpo_id"] = alamut["HPO Id (from Clinvar)"]
+            self.new_variant_data["medgen_id"] = alamut["Medgen Id (from Clinvar)"]
+            self.new_variant_data["mondo_id"] = alamut["Mondo Id (from Clinvar)"]
+            self.new_variant_data["omim_id"] = alamut["OMIM Id (from Clinvar)"]
+            self.new_variant_data["orphanet_id"] = alamut["Orphanet Id (from Clinvar)"]
+            self.new_variant_data["uniprot_id"] = alamut["Uniprot Id"]
+            self.new_variant_data["rs"] = alamut["dbSNP rsId"]
+            """
+
         
         if len(urls) == 4:
             self.complete = True
