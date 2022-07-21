b600458f7f0951d33ecee33f5b539b604986fa6d
David van Zessen <d.vanzessen@erasmusmc.nl>
2022-07-21 15:13:09 +0200

    better varsome code, but can't really use it because request limit ?

--- sources/varsome.py
+++ sources/varsome.py
@@ -8,16 +8,21 @@ class Varsome(Source):
             ("transcript", "cdot"): self.transcript_cdot
         }
 
+    def process(self, url):
+        
+        self.html_links["transcript cdot"] = SourceURL("Transcript:cdot", url)
+
     async def rs(self):
         rs = self.variant["rs"]
         url = f"https://varsome.com/variant/search?query={rs}&genome=hg19&annotation-mode=germline"
         
-        self.html_links["rs"] = SourceURL("RS", url)
+        self.process(url)
 
     async def transcript_cdot(self):
         transcript = self.variant['transcript']
         cdot = self.variant['cdot']
         transcript_cdot = f"{transcript}:{cdot}"
         url = f"https://varsome.com/variant/search?query={transcript_cdot}&genome=hg19&annotation-mode=germline"
-        self.html_links["transcript cdot"] = SourceURL("Transcript:cdot", url)
+
+        self.process(url)
 
