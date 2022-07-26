2ce89cc219f7cf86e0b08f065247888ae143286a
David van Zessen <d.vanzessen@erasmusmc.nl>
2022-07-26 16:51:10 +0200

    just import * for sources in config

--- config.py
+++ config.py
@@ -1,20 +1,7 @@
-from typing import List, Tuple
+from typing import List
 from pydantic import BaseSettings, Field
 
-from typing import Callable
-from sources import Alamut
-from sources import dbSNP
-from sources import Franklin
-from sources import COSMIC
-from sources import ClinVar
-from sources import LOVD
-from sources import cBioPortal
-from sources import CKB
-from sources import OncoKB
-from sources import PMKB
-from sources import TP53
-from sources import Varsome
-from sources import HMF
+from sources import *
 
 class Settings(BaseSettings):
     debug: bool = Field(True, env="DEBUG")
@@ -32,6 +19,7 @@ class Settings(BaseSettings):
         cBioPortal,
         Varsome,
         HMF,
+        Mutalyzer,
         TP53,
         # PMKB,
     ]
