c4ec5a4d9430fb6b5104de9701286b92f0fab424
David van Zessen <d.vanzessen@erasmusmc.nl>
2022-07-28 10:20:35 +0200

    improved lovd variant search

--- sources/lovd.py
+++ sources/lovd.py
@@ -1,3 +1,4 @@
+from html import escape
 from lxml import etree
 from .source_result import Source, SourceURL
 
@@ -12,12 +13,12 @@ class LOVD(Source):
         gene = self.variant["gene"]
         url = f"https://databases.lovd.nl/shared/variants/{gene}/unique"
 
-        self.html_links["main"] = SourceURL("Go", url)
+        self.html_links["main"] = SourceURL("Gene", url)
 
     async def gene_cdot(self):
         gene = self.variant["gene"]
         url = f"https://databases.lovd.nl/shared/variants/{gene}/unique"
-        self.html_links["main"] = SourceURL("Go", url)
+        self.html_links["main"] = SourceURL("Gene", url)
         
         cdot = self.variant["cdot"]
 
@@ -31,30 +32,13 @@ class LOVD(Source):
         transcript_id_input = transcript_id_input[0]
         transcript_id = transcript_id_input.get("value")
 
-        view_list_id = f"CustomVL_VOTunique_VOG_{gene}"
-        objct = "Custom_ViewList"
-        object_id = "VariantOnTranscriptUnique,VariantOnGenome"
-        page = 1
-        cdot_url = None
-        encoded_cdot = cdot.replace("&", "&gt;")
-        while not cdot_url:
-            table_url = f"https://databases.lovd.nl/shared/ajax/viewlist.php?viewlistid={view_list_id}&object={objct}&object_id={object_id}&id={gene}&search_transcriptid={transcript_id}&page_size=1000&page={page}"
-            resp, lovd_page = await self.async_get_text(table_url)
-            tree = etree.HTML(bytes(lovd_page, encoding="utf8"))
-            any_rows_on_page = tree.xpath("//tr[@data-href]")
-            if not any_rows_on_page:
-                break
+        encoded_cdot = escape(cdot)
+        variant_url = f"https://databases.lovd.nl/shared/variants/{gene}?search_vot_clean_dna_change={encoded_cdot}&search_transcriptid={transcript_id}&page_size=1000"
 
-            cdot_link = tree.xpath(f"//a[text()='{encoded_cdot}']")
-            if cdot_link:
-                cdot_link = cdot_link[0]
-                cdot_url = f"https://databases.lovd.nl/shared/{cdot_link.get('href')}"
-                print(cdot_url)
-                break
-            page += 1
-        if cdot_url:
-            self.html_links["Gene"] = SourceURL("Gene", self.html_links["main"].url)
-            self.html_links["main"] = SourceURL("Go", cdot_url) 
-        #  https://databases.lovd.nl/shared/ajax/viewlist.php?viewlistid=CustomVL_VOTunique_VOG_PIK3CA&object=Custom_ViewList&object_id=VariantOnTranscriptUnique%2CVariantOnGenome&id=PIK3CA&order=VariantOnTranscript%2FDNA%2CASC&search_transcriptid=00016173&page_size=1000&page=1
-                
-        # //tr[@data-href]
+        resp, variant_page = await self.async_get_text(variant_url)
+        any_rows_on_page = variant_page.count("data-href")
+        if any_rows_on_page < 0:
+            self.log_warning(f"No rows found for '{cdot}'")
+            self.html_text = "Variant not found"
+        else:
+            self.html_links["variant"] = SourceURL("Variant", variant_url)
