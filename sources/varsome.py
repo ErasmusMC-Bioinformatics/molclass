38c14e5aec6471ae41081f1475624f7e47b93d68
David van Zessen <d.vanzessen@erasmusmc.nl>
2022-08-02 16:18:56 +0200

    Label fix in varsome link

--- sources/varsome.py
+++ sources/varsome.py
@@ -8,15 +8,15 @@ class Varsome(Source):
             ("transcript", "cdot"): self.transcript_cdot
         }
 
-    def process(self, url):
+    def process(self, url, label="Go"):
         
-        self.html_links["transcript cdot"] = SourceURL("Transcript:cdot", url)
+        self.html_links["main"] = SourceURL(label, url)
 
     async def rs(self):
         rs = self.variant["rs"]
         url = f"https://varsome.com/variant/search?query={rs}&genome=hg19&annotation-mode=germline"
         
-        self.process(url)
+        self.process(url, "rs")
 
     async def transcript_cdot(self):
         transcript = self.variant['transcript']
@@ -24,5 +24,5 @@ class Varsome(Source):
         transcript_cdot = f"{transcript}:{cdot}"
         url = f"https://varsome.com/variant/search?query={transcript_cdot}&genome=hg19&annotation-mode=germline"
 
-        self.process(url)
+        self.process(url, "transcript:cdot")
 
