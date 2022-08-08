216983f0cac1c6188fba7fe58174a18f9b2b3956
David van Zessen <d.vanzessen@erasmusmc.nl>
2022-08-08 14:49:37 +0200

    dbsnp displays pop freqs, bit big but not really any other way possible?

--- sources/dbsnp.py
+++ sources/dbsnp.py
@@ -1,6 +1,8 @@
 import re
+from bs4 import BeautifulSoup
 
 from fastapi.templating import Jinja2Templates
+from jinja2 import BaseLoader, Environment
 
 from .source_result import Source, SourceURL
 
@@ -11,13 +13,54 @@ ALLELES_RE = re.compile("<dt>\s*Alleles</dt>[\s\n]+<dd>([^<]+)</dd>", re.IGNOREC
 
 GENE_CONSEQUENCE_RE = re.compile("<dt>Gene\s*:\s*Consequence</dt>\s*<dd>\s*(<div>|<span>)(?P<gene>[^ ]+)\s*:\s*(?P<consequence>[^<]+)", re.IGNORECASE)
 
+FREQ_TABLE_TEMPLATE = """
+<table class='table table-sm'>
+{% for _, row in freq_dict.items() %}
+    <tr>
+        <td>{{ row.study }}</td>
+        <td>{{ row.size }}</td>
+        <td>{{ row.ref }}</td>
+        <td>{{ row.alts }}</td>
+    </tr>
+{% endfor %}
+</table>
+"""
+
 class dbSNP(Source):
     def set_entries(self):
         self.entries = {
             ("rs",): self.rs
         }
 
-    def process(self, text):
+    def get_freq_table(self, dbsnp_text):
+        soup = BeautifulSoup(dbsnp_text, "html.parser")
+
+        freq_table_div = soup.find("table", {"id": "dbsnp_freq_datatable"})
+        if not freq_table_div:
+            self.log_info("No Clinical sign table")
+            return ""
+        clinical_sign_tbody = freq_table_div.find("tbody")
+
+        freq_dict = {}
+        for row in clinical_sign_tbody.find_all("tr"):
+            cols = row.find_all("td")
+            study, population, group, size, ref, alts = [e.text.strip() for e in cols]
+            if population not in ["Global", "Total"]:
+                continue
+            
+            freq_dict[(study, population)] = {
+                "study": study,
+                "population": population,
+                "group": group,
+                "size": size,
+                "ref": ref,
+                "alts": alts,
+            }            
+
+        template = Environment(loader=BaseLoader).from_string(FREQ_TABLE_TEMPLATE)
+        return template.render(freq_dict=freq_dict)
+
+    async def process(self, text):
         """
         allele_match = ALLELES_RE.search(text)
         if allele_match:
@@ -32,6 +75,9 @@ class dbSNP(Source):
         if m := GENE_CONSEQUENCE_RE.search(text):
             self.new_variant_data.update(**m.groupdict())
 
+        resp, dbsnp_text = await self.async_get_text(self.url)
+        self.html_text = self.get_freq_table(dbsnp_text)
+
         self.complete = True
 
 
@@ -41,4 +87,4 @@ class dbSNP(Source):
         self.url = f"https://www.ncbi.nlm.nih.gov/snp/{rs}"
         response, text = await self.async_get_text(self.url)
 
-        self.process(text)    
+        await self.process(text)    
