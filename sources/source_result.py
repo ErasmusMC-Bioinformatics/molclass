00d978adb7ce58f74abb23e6cf3bd0e6a1b26b48
Dennis Dollée <no.ploidy@pm.me>
2025-09-01 15:03:26 +0200

    Cleanup Source class

--- sources/source_result.py
+++ sources/source_result.py
@@ -3,102 +3,89 @@ import traceback
 import inspect
 from typing import List
 
-
 import aiohttp
+from icecream import ic
+
+ic.configureOutput(prefix="debug-", includeContext=True)
 
 from templates import templates
+
 
 class SourceURL:
     """
     Convenience class to style the urls on the source cards on the client webpage
     Optionally can set self.override to whatever to change the styling/behavior
     """
+
     def __init__(self, text, url, override=""):
         self.text = text
         self.url = url
         self.override = override
-    
+
     def __str__(self):
         if self.override:
             return self.override
         else:
             return f"<a target='_blank' class='btn btn-primary btn-sm my-1' href='{self.url}'>{self.text}</a>"
 
+
 class Source:
+    """
+    A class representing a source with its meta data and logs.
+
+    Attributes:
+    variant (dict): A read-only copy of the current meta data.
+    consensus (dict): The consensus meta data.
+    logs (List[dict]): Holds the logs for this source for this iteration, should not be used directly, use the methods.
+    executed (bool): Is True only after the source's entry executes successfully.
+    complete (bool): If true at the end of an iteration, the source is considered 'complete', meaning no more meta data can be gotten from the source. If false, the source will be included again in the next iteration checking the next entry.
+    error (bool): Indicates if an error occurred during this iteration.
+    timeout (bool): Indicates if the source timed out.
+    found (bool): Should be set to True if there was no result found in the source database.
+    entries (dict): Holds the 'entries', a ((key), func). See set_entries() for more info.
+    new_variant_data (dict): Add new meta data to this.
+    matches_consensus (bool): Indicates if this source matches with the consensus.
+    matches_consensus_tooltip (List[str]): A list of tooltips that will be displayed on the source on the client.
+    current_entry (Tuple): Stores the entry that is currently being executed so it can possibly be restored.
+    html_title (str): The title of the source card on the client, defaults to self.get_name().
+    html_subtitle (str): An optional subtitle to the source card on the client.
+    html_text (str): The body of the source card on the client.
+    html_links (Dict): A dict of SourceURLs that will be added to the footer of the source card on the client.
+    """
+
     def __init__(self, variant, consensus):
         self.variant: dict = variant
-        """A read only copy of the current meta data"""
-        
         self.consensus: dict = consensus
-        """The consensus meta data"""
-
         self.logs: List[dict] = []
-        """Holds the logs for this source for this iteration, should not be used directly, use the methods"""
-        
         self.executed: bool = False
-        """Is True only after the sources entry executes succesfully"""
-
         self.complete: bool = False
-        """
-        If it's true at the end of an iteration the source is considered 'complete',
-        as in, no more meta data can be gotten from the source.
-        If left on False, the source will be included again in the next iteration checking checking the next entry
-        """
-        
         self.error: bool = False
-        """Did an error occur during this iteration?""" 
-
         self.timeout: bool = False
-        """Did the source time out?"""
-
         self.found: bool = True
-        """Should be set to True if there was no result found in the source database"""
-
         self.entries: dict = {}
-        """
-        Holds the 'entries', a ((key), func).
-        See set_entries() for more info
-        """
-
         self.new_variant_data = {}
-        """Add new meta data to this"""
-
         self.matches_consensus = True
-        """Does this source match with the consensus?"""
-
         self.matches_consensus_tooltip = []
-        """A list of tooltips that will be displayed on the source on the client"""
-
         self.current_entry = ()
-        """Stores the entry that is currently being executed so it can possibly be restored"""
-
         self.html_title = self.get_name()
-        """The title of the source card on the client, defaults to self.get_name()"""
-
         self.html_subtitle = ""
-        """An optional subtitle to the source card on the client"""
-
         self.html_text = ""
-        """The body of the source card on the client"""
-
         self.html_links = {}
-        """A dict of SourceURLs, that will be added to the footer of the source card on the client"""
-        
         self.set_entries()
-
         self.name = self.get_name()
-        """self.get_name()"""
 
     def set_entries(self) -> dict:
         """
-        Should set self.entries to a dict of tuple -> func: 
+        Should set self.entries to a dict of tuple -> func:
         self.entries = {
             ('transcript', 'cdot'): self.transcript_cdot,
             ('gene',): self.gene, # Note the ',' even though it's only one tag
         }
         And then implementing self.transcript_cdot/self.gene to whatever queries the source with that meta datas
         """
-        raise NotImplementedError(f"Need to implement 'set_entries' for {self.__class__.__name__}")
+        raise NotImplementedError(
+            f"Need to implement 'set_entries' for {self.__class__.__name__}"
+        )
 
     def is_hidden(self) -> bool:
         """Can be used to hide a card on the client, useful for sources that are just there to increase consensus"""
@@ -125,7 +112,7 @@ class Source:
         self.matches_consensus_tooltip = []
 
         entry = self.get_entry()
-        
+
         if self.error:  # something wrong with entries
             return self
         if not entry:
@@ -148,7 +135,6 @@ class Source:
             print(f"Error {self}\n", "".join([m["message"] for m in self.logs]))
         return self
 
-
     def get_entry(self):
         """
         Goes through self.entries and returns the first entry that can be fulfilled with the current metadat
@@ -167,7 +153,7 @@ class Source:
 
     def restore_entry(self):
         """
-        If an entry isn't completed succesfully but might on the next iteration, 
+        If an entry isn't completed succesfully but might on the next iteration,
         restore it so it can be called again
         """
         self.log_info("Restoring entry")
@@ -179,18 +165,16 @@ class Source:
         Uses the Jinja2 card.html.jinja2 template to render this source to html
         """
         return templates.get_template(
-            "card.html.jinja2", 
+            "card.html.jinja2",
         ).render(source=self)
 
     def log(self, message, level="info"):
         """
         Generic log function, should probably use the other logs functions (log_debug, log_info, etc)
         """
-        self.logs.append({
-            "level": level,
-            "source": self.get_name(),
-            "message": message
-        })
+        self.logs.append(
+            {"level": level, "source": self.get_name(), "message": message}
+        )
 
     def log_debug(self, message):
         """self.log(message, level='debug')"""
@@ -199,7 +183,7 @@ class Source:
     def log_info(self, message):
         """self.log(message, level='info')"""
         self.log(message, level="info")
-    
+
     def log_warning(self, message):
         """self.log(message, level='warning')"""
         self.log(message, level="warning")
@@ -246,10 +230,10 @@ class Source:
         except asyncio.TimeoutError:
             self.timeout = True
             raise TimeoutError()
-    
+
     async def async_post_json(self, url, *args, **kwargs):
         """Uses the aiohttp pool to perform an asynchronous http POST request, returning it as parsed json"""
-        try: 
+        try:
             self.log_debug(f"post_json: {url}")
             async with self.session.post(url, *args, **kwargs) as response:
                 resp = await response.json()
